dd53d2eef55a526816a817705ce96768acd22616
andres sevilla <baddie.memo@gmail.com>
2025-05-11 14:04:28 -0500

    Delete contador_de _clicks directory

--- contador_de _clicks/script.js
+++
@@ -1,9 +0,0 @@
-let contador = 0;
-
-const boton = document.getElementById("clickButton");
-const displayContador = document.getElementById("contador");
-
-boton.addEventListener("click", function() {
-    contador++; 
-    displayContador.innerText = contador;
-});
