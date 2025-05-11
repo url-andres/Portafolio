0b27627a4074dcbe4ba16bbedeae5808f4377134
andres sevilla <baddie.memo@gmail.com>
2025-05-11 14:04:44 -0500

    Delete convertidor_a_mayuculas directory

--- convertidor_a_mayuculas/script.js
+++
@@ -1,6 +0,0 @@
-function convertirMayusculas() {
-    const textoEntrada = document.getElementById("textoEntrada").value;
-    const textoMayusculas = textoEntrada.toUpperCase();
-    document.getElementById("textoSalida").textContent = textoMayusculas;
-  }
-  
